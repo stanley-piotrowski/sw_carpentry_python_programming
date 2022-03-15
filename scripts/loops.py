2f4a0dbd85aee9637f869f471d77fba278873028
Stanley Piotrowski <stanleyjpiotrowski@gmail.com>
2022-03-15 12:30:28 -0700

    finished exercises

--- scripts/loops.py
+++ scripts/loops.py
@@ -1,6 +1,9 @@
 # loops.py ----
 # This script focuses on using loops for iterating over data structures to perform common operations
 
+# Setup
+import numpy
+
 # For loops ----
 # Basic example of using a for loop
 odds = [1, 3, 5, 7, 9, 11]
@@ -32,5 +35,48 @@ for i in range(len(word)):
     iterations += 1
 
 print(f'Total number of iterations: {iterations}') # 6
+
+# Exercise -- write a loop that calculates powers using multiplication
+# The exercise is 5 ** 3, which should equal 125
+num = 5
+power = 3
+total = num
+for i in range(1, power):
+    total *= num
+
+print(num) # 125
+
+# Exercise-- summing a list
+# Add each element and print the final value
+# E.g. [124, 402, 36] summed together is 562
+nums = [124, 402, 36]
+total = 0
+for num in nums:
+    total += num
+
+print(f'Sum of numbers calculated with a loop: {total}')
+
+# You can also use numpy.sum() to do the same computation
+print(f'Sums of numbers calculated using numpy: {numpy.sum(nums)}')
+
+# Enumerate -- creates a new sequence pair with the original list and the indexes
+# For example, using the nums list
+for idx, val in enumerate(nums):
+    print(f'Index: {idx}, value: {val}')
+
+# Exercise -- write a solution using loops and enumerate() which computes the value of y at any polynomial given x and coefs
+coefs = [2, 4, 3]
+x = 5
+
+y = []
+for idx, coef in enumerate(coefs):
+    y.append(coef * x**idx)
+
+# Print the term and value of y at each polynomial
+for idx, val in enumerate(y):
+    print(f'Polynomial: {idx + 1}; value of y: {val}')
+
+
+
 
 
